4597226875ee07711b528c31715ccc2539b8e531
Danis Music <danism@student.chalmers.se>
2024-05-02 17:32:06 +0200

    Issue#42 Create preliminary tests for Current_Date class
    Co-authored-by: Stefan stefantr@student.chalmers.se

---
+++ src/test/java/fitVenture/CurrentDateTest.java
@@ -0,0 +1,37 @@
+package fitVenture;
+
+import fitVenture.backend.utils.Current_Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test; 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CurrentDateTest { 
+
+    @Test
+    @DisplayName("Tests if Current_Date.getIntegerOfSpecificDate method handles different date inputs.")
+    public void getIntegerOfSpecificDateTest() {
+        assertEquals(20041215, Current_Date.getIntegerOfSpecificDate("2004-12-15 16"), "The method should convert the string date into an integer format.");
+        assertEquals(20041215, Current_Date.getIntegerOfSpecificDate("2004-12-15 16"), "The method should convert the string date into an integer format.");
+
+        //Expecting failure
+        assertEquals(-1, Current_Date.getIntegerOfSpecificDate("This should not be allowed"), 
+        "The method should not allow any symbols or characters that are not integers(1, 2, 3, ...) and dashes (-).");
+        
+        Throwable exception = assertThrows(StringIndexOutOfBoundsException.class, ()-> Current_Date.getIntegerOfSpecificDate("2004"));
+        assertEquals("A complete date format has to be provided.", exception.getMessage());
+
+
+    }
+
+    @Test
+    @DisplayName("Tests if Current_Date.getDay method handles different inputs for the day")
+    public void getDayTest() {
+        Throwable exception = assertThrows(StringIndexOutOfBoundsException.class, ()-> Current_Date.getDay(2004));
+        assertEquals("A complete date format has to be provided.", exception.getMessage());
+    }
+
+
+
+}
