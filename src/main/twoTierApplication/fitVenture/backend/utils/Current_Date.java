6066049cba8d3837f928affdba4b003976af0e95
Danis Music <danism@student.chalmers.se>
2024-05-10 22:00:49 +0200

    Issue#44 Add space as an allowed character

--- src/main/twoTierApplication/fitVenture/backend/utils/Current_Date.java
+++ src/main/twoTierApplication/fitVenture/backend/utils/Current_Date.java
@@ -12,7 +12,7 @@ public class Current_Date {
 
     // Returns specific date as an integer
     public static int getIntegerOfSpecificDate(String date) {
-        Set<Character> allowedCharacters = new HashSet<>(Arrays.asList('-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+        Set<Character> allowedCharacters = new HashSet<>(Arrays.asList(' ','-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
         String newDate = "";
         /*Set<Integer> setOfLine = new HashSet<Integer>();
         setOfLine.add(4);
